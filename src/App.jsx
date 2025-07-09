18136daae306742fa002b45a14fe260466916e8c
Joy Obaidu <joyobaidu@gmail.com>
2025-07-09 16:57:27 +0100

    edited login name

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,4 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import React from 'react';
 import Welcome from './pages/Welcome';
 import Login from './pages/SignUp';
 import Dashboard from './pages/Dashboard';
